c84e8f25878f5ba894426efe979bab04d3422509
Santhosh20234354 <132885400+Santhosh20234354@users.noreply.github.com>
2024-12-17 22:52:59 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5656/api/employees'
+    baseURL: 'https://employeeform-backend-saaq.onrender.com'
 });
 
 export const addEmployee = (data) => api.post('/', data);
